7a88693e300a7560ee62a780bca42f47002b12fb
John Valle <vallejohn.vj@gmail.com>
2021-09-13 11:17:21 +0800

    Add logic, hid content when loading

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -38,71 +38,75 @@ export default function Dashboard() {
         <Text fontSize="4xl" fontWeight="bold" pt="2rem">
           Products
         </Text>
-        <Box mb="1.5rem">
-          <Divider />
-          <Box my="0.5rem">
-            <Box>
-              <Text mb="0.5rem">Categories</Text>
-              <Flex flexWrap="wrap">
-                {categories.data?.data?.map((category: string[]) => (
-                  <Box
-                    py="0.25rem"
-                    px="0.5rem"
-                    mr="0.5rem"
-                    bgColor="white"
-                    rounded="lg"
-                    borderColor="gray.200"
-                    borderWidth="1px"
-                    _hover={{ borderColor: "blue.200", cursor: "pointer" }}
+
+        {products.isLoading && <Text>Loading products...</Text>}
+
+        {!products.isLoading && (
+          <Box mb="1.5rem">
+            <Divider />
+            <Box my="0.5rem">
+              <Box>
+                <Text mb="0.5rem">Categories</Text>
+                <Flex flexWrap="wrap">
+                  {categories.data?.data?.map((category: string[]) => (
+                    <Box
+                      py="0.25rem"
+                      px="0.5rem"
+                      mr="0.5rem"
+                      bgColor="white"
+                      rounded="lg"
+                      borderColor="gray.200"
+                      borderWidth="1px"
+                      _hover={{ borderColor: "blue.200", cursor: "pointer" }}
+                    >
+                      <Text fontSize="md">{category}</Text>
+                    </Box>
+                  ))}
+                </Flex>
+              </Box>
+              <Flex maxWidth="350px" my="1rem">
+                <FormControl as="fieldset">
+                  <FormLabel as="legend">Sort products by</FormLabel>
+                  <RadioGroup
+                    defaultValue="asc"
+                    onChange={(value: SortByValue) => {
+                      setProductConfig((prevState) => ({
+                        ...prevState,
+                        sort: value,
+                      }));
+                    }}
                   >
-                    <Text fontSize="md">{category}</Text>
-                  </Box>
-                ))}
+                    <HStack spacing="24px">
+                      <Radio value="asc">Ascending</Radio>
+                      <Radio value="desc">Descending</Radio>
+                    </HStack>
+                  </RadioGroup>
+                </FormControl>
+                <FormControl id="amount" maxWidth={{ sm: "100%", md: "100px" }}>
+                  <FormLabel>Amount</FormLabel>
+                  <NumberInput
+                    max={20}
+                    min={1}
+                    step={1}
+                    defaultValue={10}
+                    onChange={(value: string) => {
+                      setProductConfig((prevState) => ({
+                        ...prevState,
+                        limit: Number(value),
+                      }));
+                    }}
+                  >
+                    <NumberInputField bg="white" />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </FormControl>
               </Flex>
             </Box>
-            <Flex maxWidth="350px" my="1rem">
-              <FormControl as="fieldset">
-                <FormLabel as="legend">Sort products by</FormLabel>
-                <RadioGroup
-                  defaultValue="asc"
-                  onChange={(value: SortByValue) => {
-                    setProductConfig((prevState) => ({
-                      ...prevState,
-                      sort: value,
-                    }));
-                  }}
-                >
-                  <HStack spacing="24px">
-                    <Radio value="asc">Ascending</Radio>
-                    <Radio value="desc">Descending</Radio>
-                  </HStack>
-                </RadioGroup>
-              </FormControl>
-              <FormControl id="amount" maxWidth={{ sm: "100%", md: "100px" }}>
-                <FormLabel>Amount</FormLabel>
-                <NumberInput
-                  max={20}
-                  min={1}
-                  step={1}
-                  defaultValue={10}
-                  onChange={(value: string) => {
-                    setProductConfig((prevState) => ({
-                      ...prevState,
-                      limit: Number(value),
-                    }));
-                  }}
-                >
-                  <NumberInputField bg="white" />
-                  <NumberInputStepper>
-                    <NumberIncrementStepper />
-                    <NumberDecrementStepper />
-                  </NumberInputStepper>
-                </NumberInput>
-              </FormControl>
-            </Flex>
           </Box>
-        </Box>
-        {products.isLoading && <Text>Loading products...</Text>}
+        )}
         <SimpleGrid minChildWidth="180px" spacing="1.5rem">
           {products.data?.data?.map((product: IProduct) => (
             <Link key={product.id} to={`/products/${product.id}`}>
