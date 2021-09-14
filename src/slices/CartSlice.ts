e3bee0df381d68577821a14348acedeec7da3ea6
John Valle <vallejohn.vj@gmail.com>
2021-09-14 14:46:51 +0800

    Modify add product type

--- src/slices/CartSlice.ts
+++ src/slices/CartSlice.ts
@@ -1,20 +1,21 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ICartProduct } from "../constants";
 
 export const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    products: [] as number[],
+    products: [] as ICartProduct[],
     quantity: 0,
   },
   reducers: {
-    addProduct: (state, action: PayloadAction<number>) => {
-      const productId = action.payload;
-      const newProducts = [...state.products, productId];
+    addProduct: (state, action: PayloadAction<ICartProduct>) => {
+      const product = action.payload;
+      const newProducts = [...state.products, product];
       state.products = newProducts;
     },
     removeProduct: (state, action: PayloadAction<number>) => {
       const productId = action.payload;
-      const newProducts = state.products.filter((id) => id !== productId);
+      const newProducts = state.products.filter((product) => product.id !== productId);
       state.products = newProducts;
     },
     setQuantity: (state, action: PayloadAction<number>) => {
